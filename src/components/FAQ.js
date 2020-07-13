69f51699ee4663f6b1f470d0fbf30de10f5e699c
Kai Quander <kq1003@gmail.com>
2020-07-13 16:27:58 -0400

    FAQs page about done

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -9,9 +9,31 @@ class FAQ extends Component {
                     FAQ's Page
                 </div>
                 <br/>
-            </div>
-                    );
-                }
-            }
+                <div class="content">
+                    <div>
+                            <label class="question">
+                            What is this page?
+                            </label>
+                            <div class="answers">
+                            This is the FAQ's page, so that your questions can be answered about our website!
+                            </div>
+                            <label class="question">
+                            Who made this page?
+                            </label>
+                            <div class="answers">
+                            Lorel, Luis and Kai made this page
+                            </div>
+                            <label class="question">
+                            Why was this page made?
+                            </label>
+                            <div class="answers">
+                            We made this page to promote healthy living and healthy eating habits!
+                            </div>
+                    </div>
+                </div>
+               </div> 
+            );
+    }
+}
 
 export default FAQ;
