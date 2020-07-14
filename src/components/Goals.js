9a9fd49ff04d5b500bd56d655bfe8fdf6d321c22
lorelpayano <lorelpayano@hotmail.com>
2020-07-14 12:56:08 -0400

    fixed goals in home

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -7,30 +7,28 @@ class Goals extends Component {
     render() {
         return (
             <div class = 'success'>
-                <p><h2>We have the items for your success!</h2></p><br/>
-                <section id="general-information">
-                <div class="containerGoals">
+                <h2>We have the items for your success!</h2><br/>
+                <section id ="general-information">
+                    <div class="containerGoals">
+                        <article class="col col-3">
+                        <h3>Eat right, feel good!</h3>
+                        <p class="text">With out ingredient tracker, you will be able to easily track what you are eating, making healthy eating so much easier!</p>
+                        <img src={eating} width = '150' height = '100' alt='goal'/>
+                        </article>
 
-                    <article class="col col-3">
-                    <h3>Eat right, feel good!</h3>
-                    <p class="text">With out ingredient tracker, you will be able to easily track what you are eating, making healthy eating so much easier!</p>
-                    <img src={eating} width = '150' height = '100' />
-                    </article>
+                        <article class="col col-3">
+                        <h3>Healthy ingredients, happy life</h3>
+                        <p class="text">Get healthy recipes with out recipe tracker</p>
+                        <img src={recipes} width = '150' height = '100' alt='goal'/>
+                        </article>
 
-                    <article class="col col-3">
-                    <h3>Healthy ingredients, happy life</h3>
-                    <p class="text">Get healthy recipes with out recipe tracker</p>
-                    <img src={recipes} width = '150' height = '100' />
-                    </article>
-
-                    <article class="col col-3">
-                    <h3>Excercise more!</h3>
-                    <p class="text">Get excercises that work for you and that you can track whenever you want! </p>
-                    <img src={excercise} width = '150' height = '100' />
-                    </article>
+                        <article class="col col-3">
+                        <h3>Excercise more!</h3>
+                        <p class="text">Get excercises that work for you and that you can track whenever you want! </p>
+                        <img src={excercise} width = '150' height = '100' alt='goal' />
+                        </article>
                     </div>
-  
-</section>
+                </section>
             </div>
         );
     }
